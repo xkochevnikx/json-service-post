6f098701610a18f2534a68eaeade224b795ce070
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-07-24 13:23:54 +0300

    add AppRouter

---
+++ src/app/router/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routes } from './routes';
+
+const AppRouter = () => {
+    return (
+        <Routes>
+            {routes.map((route) => (
+                <Route
+                    key={route.path}
+                    element={route.component}
+                    path={route.path}
+                />
+            ))}
+        </Routes>
+    );
+};
+
+export default AppRouter;
---
+++ src/app/router/routes.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const routes = [
+    { path: '/:page', component: <PostsListPage /> },
+];
