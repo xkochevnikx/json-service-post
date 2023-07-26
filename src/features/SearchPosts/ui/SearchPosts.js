a090d57f95a6449777b59a874f18bb0a97d8dfec
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-07-26 14:01:28 +0300

    add hook useDebounce

--- src/features/SearchPosts/ui/SearchPosts.js
+++ src/features/SearchPosts/ui/SearchPosts.js
@@ -3,6 +3,7 @@ import { MyInput } from '../../../shared/ui/MyInput/MyInput';
 import { useDispatch } from 'react-redux';
 import { searchSliceActions } from '../modal/slice/searchSlice';
 import cls from './SearchPosts.module.css'
+import { useDebounce } from '../../../shared/lib/hooks/useDebounce';
 
 
 export function SearchPosts () {
@@ -13,9 +14,11 @@ export function SearchPosts () {
         dispatch(searchSliceActions.setText(e.target.value))
     }
 
+    const debounce = useDebounce(onHandlerInput, 500)
+
     return (
         <form className={cls.search}>
-            <MyInput onChange={onHandlerInput} placeholder="Поиск"/>
+            <MyInput onChange={debounce} placeholder="Поиск"/>
             <MyInput type='submit' className={cls.btnSubmit} value=" "/> 
         </form>
         
