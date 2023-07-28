9bed3b5e1ea078ffb8add52cdf581b7e74e13937
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-07-28 14:00:38 +0300

    fix test searchSlice.test

--- src/features/SearchPosts/modal/slice/searchSlice.test.js
+++ src/features/SearchPosts/modal/slice/searchSlice.test.js
@@ -4,7 +4,14 @@ import {
 } from './searchSlice';
 
 describe('searchSlice.test', () => {
-    test('search test', () => {
+    test('should return state when passed an empty action', () => {
+        expect(searchSliceReducer(undefined, {})).toEqual({
+            text: '',
+            error: '',
+        });
+    });
+
+    test('search test action', () => {
         const state = { text: '' };
         expect(
             searchSliceReducer(
